505ee1d91de782d3e69012f1db49871a373fc6ce
UsamaAsghar <usama_a123@hotmail.com>
2020-07-28 16:05:24 -0400

    added some JS :sunglasses:

---
+++ styles/assets/scripts/scripts.js
@@ -0,0 +1,12 @@
+// Creating a namespace to store code
+const poke = {};
+
+// Organizing init function to pass to document ready
+poke.init = () => {
+
+}
+
+// Document ready function
+$(function () {
+    poke.init();
+});
